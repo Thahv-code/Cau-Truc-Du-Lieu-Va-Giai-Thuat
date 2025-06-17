2a2423b26c22821b344ca1893a25a3be38e73d94
Thahv-code <vuthanhnguyen946@gmail.com>
2025-06-17 15:42:43 +0700

    first commit

---
+++ bai1.c
@@ -0,0 +1,8 @@
+//
+// Created by Thanh on 17/06/2025.
+//
+#include <stdio.h>
+int main() {
+    int arr[] = {1, 2, 3, 4, 5 , 6, 7, 8};
+    int indexxDelete =
+}
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Head!\n");
+    return 0;
+}
