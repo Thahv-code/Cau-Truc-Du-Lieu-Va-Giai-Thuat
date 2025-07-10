73222a9be5b3fdb86149261d93984e0c99c055ba
Thahv-code <vuthanhnguyen946@gmail.com>
2025-07-10 23:37:02 +0700

    :PTIT_CNTT1_IT105_Session15

---
+++ Session15/bt1.c
@@ -0,0 +1,23 @@
+//
+// Created by Thanh on 10/07/2025.
+//
+#include <stdio.h>
+#define MAX 100
+
+typedef struct {
+    int arr[MAX];
+    int front;
+    int rear;
+} Queue;
+
+void initQueue(Queue *q) {
+    q->front = 0;
+    q->rear = -1;
+}
+
+int main() {
+    Queue q;
+    initQueue(&q);
+
+    return 0;
+}
